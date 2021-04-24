dd0a69b596e10cd30b7cf6b3075bbc064da9547d
abhishek-6191 <abhishekthesuperb@gmail.com>
2021-04-24 21:23:34 +0530

    simple atcoder qeustions

---
+++ Simple Implementaions and Math/A_Square_Inequality.py
@@ -0,0 +1,26 @@
+"""
+https://atcoder.jp/contests/abc199/tasks/abc199_a
+"""
+
+from sys import stdin
+input = stdin.readline
+from heapq import heapify,heappush,heappop,heappushpop
+from collections import defaultdict as dd, deque as dq,Counter as C
+from math import factorial as f ,ceil,gcd,sqrt,log
+from bisect import bisect_left as bl ,bisect_right as br
+from itertools import combinations as c,permutations as p
+from math import factorial as f ,ceil,gcd,sqrt,log
+mi = lambda : map(int,input().split())
+ii = lambda: int(input())
+li = lambda : list(map(int,input().split()))
+mati = lambda  r : [ li() for _ in range(r)]
+lcm = lambda a,b : (a*b)//gcd(a,b) 
+
+
+
+
+def solve():
+    a,b,c=mi()
+    print("Yes") if (a**2+b**2)<c**2 else print("No")
+for _ in range(1):
+    solve()
