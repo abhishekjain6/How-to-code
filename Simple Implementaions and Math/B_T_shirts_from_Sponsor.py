a084a06097d1defba3fc4413f388543affb68f60
abhishek-6191 <abhishekthesuperb@gmail.com>
2021-06-12 17:27:52 +0530

    SOme 1000 level problems

---
+++ Simple Implementaions and Math/B_T_shirts_from_Sponsor.py
@@ -0,0 +1,55 @@
+from sys import stdin,stdout
+from heapq import heapify,heappush,heappop,heappushpop
+from collections import defaultdict as dd, deque as dq,Counter as C
+from bisect import bisect_left as bl ,bisect_right as br
+from itertools import combinations as cmb,permutations as pmb
+from math import factorial as f ,ceil,gcd,sqrt,log,inf
+mi = lambda : map(int,input().split())
+ii = lambda: int(input())
+li = lambda : list(map(int,input().split()))
+mati = lambda  r : [ li() for _ in range(r)]
+lcm = lambda a,b : (a*b)//gcd(a,b) 
+MOD=10**9+7
+def soe(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    return(prime)
+def ncr(n,r):
+    p,k=1,1
+    if (n - r < r):
+        r = n - r
+    if r!=0:
+        while(r):
+            p,k=p*n,k*r
+            m=gcd(p,k)
+            p,k=p//m,k//m
+            n,r=n-1,r-1
+    else:
+            p=1
+    return(p)
+
+
+
+
+def solve():
+    n=li()
+    a={'S':0,'M':1,'L':2,'XL':3,'XXL':4}
+    b=['S','M','L','XL','XXL']
+    for i in range(int(input())):
+        c=a[input()]
+        for i in range(5):
+            if c+i<5 and n[c+i]>0:
+                print(b[c+i])
+                n[c+i]-=1
+                break
+            elif c-i>=0 and n[c-i]>0:
+                print(b[c-i])
+                n[c-i]-=1
+                break
+for _ in range(1):
+    solve()
