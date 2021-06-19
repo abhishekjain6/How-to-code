4af1c92bd0d2a0643d789b0dcd409bd43da1c96e
abhishek-6191 <abhishekthesuperb@gmail.com>
2021-06-19 22:01:15 +0530

    codeforces round 727

---
+++ Simple Implementaions and Math/A_Arithmetic_Array.py
@@ -0,0 +1,51 @@
+from sys import stdin,stdout
+from heapq import heapify,heappush,heappop,heappushpop
+from collections import defaultdict as dd, deque as dq,Counter as C
+from bisect import bisect_left as bl ,bisect_right as br
+from itertools import combinations as cmb,permutations as pmb
+from math import factorial as f ,ceil,gcd,sqrt,log,inf
+mi = lambda : map(int,input().split())
+ii = lambda: int(input())
+li = lambda : list(map(int,input().split()))
+mati = lambda  r : [ li() for _ in range(r)]
+lcm = lambda a,b : (a*b)//gcd(a,b) 
+MOD=10**9+7
+def soe(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    return(prime)
+def ncr(n,r):
+    p,k=1,1
+    if (n - r < r):
+        r = n - r
+    if r!=0:
+        while(r):
+            p,k=p*n,k*r
+            m=gcd(p,k)
+            p,k=p//m,k//m
+            n,r=n-1,r-1
+    else:
+            p=1
+    return(p)
+
+
+
+
+def solve():
+    n=ii()
+    arr=li()
+    if sum(arr)==n:
+        print(0)
+    elif sum(arr)>n:
+        print(sum(arr)-n)
+    else:
+        print(1)
+
+
+for _ in range(ii()):
+    solve()
